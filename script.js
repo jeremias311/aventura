fe64ef15a3f2277cb1980e15e0cf738894c3ecdc
jeremias311 <hernandez.jeremias@escola.pr.gov.br>
2025-09-04 10:59:43 -0300

    ok

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const avança = document.querySelectorAll('.btn-proximo');
+const avanca = document.querySelectorAll('btn-proximo');
+const reiniciarBtn = document.getElementById('btn-reiniciar');
 
-avanca.for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
-}
+avanca.array.forEach(button=>{
+    button.addEventListener('click',function() {
+     const atual= document.querySelector('.activo');
+     const proximoPasso = 'passo-'+this.getAttribbute('data-proximo');
+     
+     actual.ClassList.remove('activo');
+     const proximoElemento= document.getElementById(proximoPasso);
+   
+     if (proximoElemento){
+        proximoElemento.classList.add('activo');
+     }else{
+     console.error(`Elemento com ID"${proximoPasso}" nao encontrado.`)
+
+     }
+    });
+});
